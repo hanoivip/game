f2e4c58f02058ea806a8a27788feda019b213371
Éc Min <toannguyen@Toans-MacBook-Pro.local>
2018-03-04 16:59:02 +0700

    fix

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-//Route::namespace('Hanoivip\Game\Controllers')->get('/server-list', 'GameController@serverlist')->name('server-list');
-
 Route::middleware('web', 'auth:token')->namespace('Hanoivip\Game\Controllers')->group(function () {
     
     Route::get('/server-list', 'GameController@serverlist')->name('server-list');
