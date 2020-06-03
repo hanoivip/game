f204f4e14c0f4d0af92c3f4d3614531e97839bdc
wartune00 <wartune.mobi@gmail.com>
2020-06-03 13:24:05 +0700

    f

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['token'])->namespace('Hanoivip\Game\Controllers')->prefix('api')->group(function () {
-    Route::get('/server/list', 'GameController@serverlist');
-    Route::get('/user/role', 'GameController@queryRoles');
+    Route::any('/server/list', 'GameController@serverlist');
+    Route::any('/user/role', 'GameController@queryRoles');
     Route::post('/choose-role', 'WizardController@continue');
     // done, let move on
     Route::post('/wizard/role', 'WizardController@continue')->name('wizard.role.next');
