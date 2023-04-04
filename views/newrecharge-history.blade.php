e66e6eb00a5637e9413e510bb532067067dd6879
unknown <game.oh.vn@gmail.com>
2023-04-04 10:05:59 +0700

    g

--- views/newrecharge-history.blade.php
+++ views/newrecharge-history.blade.php
@@ -7,11 +7,12 @@
 @if (!empty($history))
 <table>
     <tr>
-        <th>Mã đơn hàng</th>
-        <th>Mã hóa đơn</th>
-        <th>T.thái thanh toán</th>
-        <th>Giá trị</th>
-        <th>T.thái nhân vật</th>
+        <th>Order</th>
+        <th>Receipt</th>
+        <th>Payment status</th>
+        <th>Payment amount</th>
+        <th>Game role status</th>
+        <th>Time</th>
     </tr>
 	@foreach ($history as $his)
 	<tr>
@@ -20,6 +21,7 @@
 		<td>{{__('hanoivip.game::newrecharge.history.status.' . $his->status)}}</td>
 		<td>{{$his->amount}}</td>
 		<td>{{__('hanoivip.game::newrecharge.history.game_status.' . $his->game_status)}}</td>
+		<td>{{__($his->created_at)}}</td>
 	</tr>
 	@endforeach
 </table>
@@ -29,7 +31,7 @@
 @endfor
 
 <br/><br/>
-<a class="btn btn-primary" href="{{route('newhistory')}}"><button>Cập nhật</button></a>
+<a class="btn btn-primary" href="{{route('newhistory')}}">Refresh</a>
 
 @else
 	<p>{{__('hanoivip.game::newrecharge.history.empty')}}</p>
