91d15cdfc9dafcbeb850b7fe3fe10143ae47af89
unknown <game.oh.vn@gmail.com>
2024-01-11 14:50:24 +0700

    f

--- routes/api.php
+++ routes/api.php
@@ -10,10 +10,6 @@ Route::middleware([
     Route::middleware('cacheByUser:60')->any('/user/role', 'GameController@queryRoles');
     Route::middleware('lockByUser:10,5')->any('/game/recharge', 'GameController@doRecharge')->name('game.recharge');
     Route::middleware('cacheByUser:60')->any('/user/all-role', 'GameController@queryRoles')->name('game.roles');
-    // payment callback
-    //Route::any('/purchase/callback', 'NewFlow@rechargeDone');
-    // payment history
-    //Route::any('/payment/history', 'NewFlow@history');
     Route::any('/wizard/save', 'WizardController@saveDefaultRole')->name('wizard.save');
 });
     
--- routes/web.php
+++ routes/web.php
@@ -11,37 +11,6 @@ Route::middleware('web', 'auth:web')->namespace('Hanoivip\Game\Controllers')->gr
     Route::get('/recharge/result-fail', 'GameController@onRechargeFail')->name('recharge.fail');
     //Util: server role wizard, can save for further
     Route::get('/wizard/role', 'WizardController@chooseRole')->name('wizard.role');
-    /*
-    //Route::get('/newrecharge', 'NewFlow@startWizard')->name('newrecharge');
-    Route::get('/newrecharge', 'NewFlow@start')->name('newrecharge');
-    Route::any('/newrecharge/shop', 'NewFlow@showShop')->name('newrecharge.shop');
-    Route::any('/newrecharge/do', 'NewFlow@recharge')->name('newrecharge.do');
-    Route::any('/newrecharge/done', 'NewFlow@rechargeDone')->name('newrecharge.done');
-    Route::any('/newrecharge/refresh', 'NewFlow@query')->name('newrecharge.refresh');
-    Route::get('/newhistory', 'NewFlow@history')->name('newhistory');
-    */
-});
-
-Route::middleware([
-    'web',
-    'admin'
-])->namespace('Hanoivip\Game\Controllers')
-->prefix('ecmin')
-->group(function () {
-    // Module index
-    //Route::get('/newrecharge', 'Admin@index')->name('ecmin.newrecharge');
-    // New flow history
-    //Route::post('/newrecharge/history', 'Admin@history')->name('ecmin.newrecharge.history');
-    // Stats
-    //Route::get('/newrecharge/stat', 'Admin@stats')->name('ecmin.newrecharge.stats');
-    //Route::any('/newrecharge/stat/today', 'Admin@today')->name('ecmin.newrecharge.statsToday');
-    //Route::any('/newrecharge/stat/month', 'Admin@thisMonth')->name('ecmin.newrecharge.statsMonth');
-    //Route::post('/newrecharge/stat', 'Admin@statByTime')->name('ecmin.newrecharge.statsByTime');
-    // Search
-    //Route::any('/newrecharge/receipt', 'Admin@viewReceipt')->name('ecmin.newrecharge.receipt');
-    // Admin trigger a receipt callback
-    //Route::post('/newrecharge/receipt/retry', 'Admin@retry')->name('ecmin.newrecharge.receipt.retry');
-    //Route::post('/newrecharge/receipt/check', 'Admin@check')->name('ecmin.newrecharge.receipt.check');
 });
 
 Route::middleware('web')->namespace('Hanoivip\Game\Controllers')->group(function () {
